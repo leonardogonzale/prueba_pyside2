35712003753cf2e6793fc0336b8aa71f12d41fb1
Leonardo González <leonardo.gonzalez3077@alumnos.udg.mx>
2020-11-09 17:22:46 -0600

    Prueba 03 y 04, creacion de la tabla y mostrar datos

--- mainwindow.py
+++ mainwindow.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QTableWidgetItem
 from PySide2.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
 from Libreria.libreria import Libreria
@@ -12,10 +12,114 @@ class MainWindow(QMainWindow):
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.agregar_final_pushButton .clicked.connect(self.click_agregar)
+        self.ui.agregar_final_pushButton.clicked.connect(self.click_agregar)
         self.ui.agregar_inicio_pushButton.clicked.connect(self.click_agregar_inicio)
         self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
 
+        self.ui.actionAbrir.triggered.connect(self.action_abrir_archivo)
+        self.ui.actionGuardar.triggered.connect(self.action_guardar_archivo)
+
+        self.ui.mostrar_tabla_pushButton_2.clicked.connect(self.mostrar_tabla)
+        self.ui.buscar_pushButton.clicked.connect(self.buscar_titulo)
+
+    @Slot()
+    def buscar_titulo(self):
+        titulo = self.ui.buscar_lineEdit.text()
+        encontrado = False
+        for libro in self.libreria:
+            if titulo == libro.titulo:
+                self.ui.tabla.clear()
+                self.ui.tabla.setRowCount(1)
+
+                titulo_widget = QTableWidgetItem(libro.titulo)
+                autor_widget = QTableWidgetItem(libro.autor)
+                publicado_widget = QTableWidgetItem(str(libro.publicado))
+                editorial_widget = QTableWidgetItem(libro.editorial)
+
+                self.ui.tabla.setItem(0, 0, titulo_widget)
+                self.ui.tabla.setItem(0, 1, autor_widget)
+                self.ui.tabla.setItem(0, 2, publicado_widget)
+                self.ui.tabla.setItem(0, 3, editorial_widget)
+
+                encontrado = True
+                return
+        if not encontrado:
+            QMessageBox.warning(
+                self,
+                "Atención",
+                f'El libro con título "{titulo}" no fue encontrado'
+            )
+        
+
+
+
+    @Slot()
+    def mostrar_tabla(self):
+        self.ui.tabla.setColumnCount(4)
+        headers = ["Titulo", "Autor", "Publicado", "Editorial"]
+        self.ui.tabla.setHorizontalHeaderLabels(headers)
+
+        self.ui.tabla.setRowCount(len(self.libreria))
+
+        row = 0
+        for libro in self.libreria:
+            titulo_widget = QTableWidgetItem(libro.titulo)
+            autor_widget = QTableWidgetItem(libro.autor)
+            publicado_widget = QTableWidgetItem(str(libro.publicado))
+            editorial_widget = QTableWidgetItem(libro.editorial)
+
+            self.ui.tabla.setItem(row, 0, titulo_widget)
+            self.ui.tabla.setItem(row, 1, autor_widget)
+            self.ui.tabla.setItem(row, 2, publicado_widget)
+            self.ui.tabla.setItem(row, 3, editorial_widget)
+
+            row += 1
+
+    @Slot()
+    def action_abrir_archivo(self):
+        #print('abrir_archivo')
+        ubicacion = QFileDialog.getOpenFileName(
+            self,
+            'Abrir archivo',
+            '.',
+            'JSON (*.json)'
+        )[0]
+        if self.libreria.abrir(ubicacion):
+            QMessageBox.information(
+                self,
+                "Éxito",
+                "Se abrió el archivo " + ubicacion
+            )
+        else:
+            QMessageBox.critical(
+                self,
+                "Error",
+                "Error al abrir el archivo " + ubicacion
+            )
+
+    @Slot()
+    def action_guardar_archivo(self):
+        #print('guardar_archivo')
+        ubicacion = QFileDialog.getSaveFileName(
+            self,
+            'Guardar Archivo',
+            '.',
+            'JSON (*.json)'
+        )[0]
+        print(ubicacion)
+        if self.libreria.guardar(ubicacion):
+            QMessageBox.information(
+                self,
+                "Éxito",
+                "Se pudo crear el archivo" + ubicacion
+            )
+        else:
+            QMessageBox.critical(
+                self,
+                "Error",
+                "No se pudo crear el archivo" + ubicacion
+            )    
+    
     @Slot()
     def click_mostrar(self):
         #self.libreria.mostrar()
--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -17,29 +17,49 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(484, 350)
+        MainWindow.resize(671, 559)
+        self.actionAbrir = QAction(MainWindow)
+        self.actionAbrir.setObjectName(u"actionAbrir")
+        self.actionGuardar = QAction(MainWindow)
+        self.actionGuardar.setObjectName(u"actionGuardar")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_3 = QGridLayout(self.centralwidget)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab_3 = QWidget()
+        self.tab_3.setObjectName(u"tab_3")
+        self.gridLayout_2 = QGridLayout(self.tab_3)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.groupBox = QGroupBox(self.centralwidget)
+        self.groupBox = QGroupBox(self.tab_3)
         self.groupBox.setObjectName(u"groupBox")
         self.gridLayout = QGridLayout(self.groupBox)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label = QLabel(self.groupBox)
-        self.label.setObjectName(u"label")
-
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-
         self.editorial_lineEdit = QLineEdit(self.groupBox)
         self.editorial_lineEdit.setObjectName(u"editorial_lineEdit")
 
         self.gridLayout.addWidget(self.editorial_lineEdit, 3, 1, 1, 1)
 
+        self.salida = QPlainTextEdit(self.groupBox)
+        self.salida.setObjectName(u"salida")
+
+        self.gridLayout.addWidget(self.salida, 0, 2, 7, 1)
+
+        self.label_2 = QLabel(self.groupBox)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+
         self.label_3 = QLabel(self.groupBox)
         self.label_3.setObjectName(u"label_3")
 
         self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+
+        self.label = QLabel(self.groupBox)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
 
         self.autor_lineEdit = QLineEdit(self.groupBox)
         self.autor_lineEdit.setObjectName(u"autor_lineEdit")
@@ -53,67 +73,109 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.publicado_spinBox, 2, 1, 1, 1)
 
-        self.titulo_lineEdit = QLineEdit(self.groupBox)
-        self.titulo_lineEdit.setObjectName(u"titulo_lineEdit")
-
-        self.gridLayout.addWidget(self.titulo_lineEdit, 0, 1, 1, 1)
-
-        self.mostrar_pushButton = QPushButton(self.groupBox)
-        self.mostrar_pushButton.setObjectName(u"mostrar_pushButton")
-
-        self.gridLayout.addWidget(self.mostrar_pushButton, 6, 0, 1, 2)
-
-        self.agregar_inicio_pushButton = QPushButton(self.groupBox)
-        self.agregar_inicio_pushButton.setObjectName(u"agregar_inicio_pushButton")
-
-        self.gridLayout.addWidget(self.agregar_inicio_pushButton, 5, 0, 1, 2)
-
-        self.label_2 = QLabel(self.groupBox)
-        self.label_2.setObjectName(u"label_2")
-
-        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
-
         self.label_4 = QLabel(self.groupBox)
         self.label_4.setObjectName(u"label_4")
 
         self.gridLayout.addWidget(self.label_4, 3, 0, 1, 1)
+
+        self.titulo_lineEdit = QLineEdit(self.groupBox)
+        self.titulo_lineEdit.setObjectName(u"titulo_lineEdit")
+
+        self.gridLayout.addWidget(self.titulo_lineEdit, 0, 1, 1, 1)
 
         self.agregar_final_pushButton = QPushButton(self.groupBox)
         self.agregar_final_pushButton.setObjectName(u"agregar_final_pushButton")
 
         self.gridLayout.addWidget(self.agregar_final_pushButton, 4, 0, 1, 2)
 
-        self.salida = QPlainTextEdit(self.groupBox)
-        self.salida.setObjectName(u"salida")
+        self.agregar_inicio_pushButton = QPushButton(self.groupBox)
+        self.agregar_inicio_pushButton.setObjectName(u"agregar_inicio_pushButton")
 
-        self.gridLayout.addWidget(self.salida, 0, 2, 7, 1)
+        self.gridLayout.addWidget(self.agregar_inicio_pushButton, 5, 0, 1, 2)
+
+        self.mostrar_pushButton = QPushButton(self.groupBox)
+        self.mostrar_pushButton.setObjectName(u"mostrar_pushButton")
+
+        self.gridLayout.addWidget(self.mostrar_pushButton, 6, 0, 1, 2)
 
 
         self.gridLayout_2.addWidget(self.groupBox, 0, 0, 1, 1)
 
+        self.tabWidget.addTab(self.tab_3, "")
+        self.tab_4 = QWidget()
+        self.tab_4.setObjectName(u"tab_4")
+        self.gridLayout_4 = QGridLayout(self.tab_4)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.tabla = QTableWidget(self.tab_4)
+        self.tabla.setObjectName(u"tabla")
+
+        self.gridLayout_4.addWidget(self.tabla, 0, 0, 1, 3)
+
+        self.buscar_lineEdit = QLineEdit(self.tab_4)
+        self.buscar_lineEdit.setObjectName(u"buscar_lineEdit")
+
+        self.gridLayout_4.addWidget(self.buscar_lineEdit, 1, 0, 1, 1)
+
+        self.buscar_pushButton = QPushButton(self.tab_4)
+        self.buscar_pushButton.setObjectName(u"buscar_pushButton")
+
+        self.gridLayout_4.addWidget(self.buscar_pushButton, 1, 1, 1, 1)
+
+        self.mostrar_tabla_pushButton_2 = QPushButton(self.tab_4)
+        self.mostrar_tabla_pushButton_2.setObjectName(u"mostrar_tabla_pushButton_2")
+
+        self.gridLayout_4.addWidget(self.mostrar_tabla_pushButton_2, 1, 2, 1, 1)
+
+        self.tabWidget.addTab(self.tab_4, "")
+
+        self.gridLayout_3.addWidget(self.tabWidget, 0, 0, 1, 1)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 484, 21))
+        self.menubar.setGeometry(QRect(0, 0, 671, 21))
+        self.menuArchivo = QMenu(self.menubar)
+        self.menuArchivo.setObjectName(u"menuArchivo")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
+        self.menubar.addAction(self.menuArchivo.menuAction())
+        self.menuArchivo.addAction(self.actionAbrir)
+        self.menuArchivo.addAction(self.actionGuardar)
+
         self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(1)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionAbrir.setText(QCoreApplication.translate("MainWindow", u"Abrir", None))
+#if QT_CONFIG(shortcut)
+        self.actionAbrir.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+O", None))
+#endif // QT_CONFIG(shortcut)
+        self.actionGuardar.setText(QCoreApplication.translate("MainWindow", u"Guardar", None))
+#if QT_CONFIG(shortcut)
+        self.actionGuardar.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+S", None))
+#endif // QT_CONFIG(shortcut)
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Libro", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Titulo", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Publicado", None))
-        self.mostrar_pushButton.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
-        self.agregar_inicio_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar inicio", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Autor", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Publicado", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Titulo", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"Editorial", None))
         self.agregar_final_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar Final", None))
+        self.agregar_inicio_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar inicio", None))
+        self.mostrar_pushButton.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), QCoreApplication.translate("MainWindow", u"Agregar", None))
+        self.buscar_lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Titulo del libro", None))
+        self.buscar_pushButton.setText(QCoreApplication.translate("MainWindow", u"Buscar", None))
+        self.mostrar_tabla_pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), QCoreApplication.translate("MainWindow", u"Tabla", None))
+        self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"Archivo", None))
     # retranslateUi
 
